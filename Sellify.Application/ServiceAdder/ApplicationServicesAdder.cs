d91f8e65ceb16a6642b8d59ba46f5e98716fcbce
Mohamed Ashraf <maelsayed4dv@gmail.com>
2026-03-05 01:35:48 -0800

    Added Authetication options to align with JWT , structured Application Folders - (added) in - infrastructure and Application

---
+++ Sellify.Application/ServiceAdder/ApplicationServicesAdder.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+using System.Reflection;
+
+namespace Sellify.Application.ServiceAdder
+{
+    public static class ApplicationServicesAdder
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddMediatR(a=> Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
--- Sellify.Infrastructure/ServicesAdder/InfrastructureServicesAdder.cs
+++ Sellify.Infrastructure/ServicesAdder/InfrastructureServicesAdder.cs
@@ -1,4 +1,8 @@
 ﻿
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 namespace Sellify.Infrastructure.ServicesAdder
 {
     public static class InfrastructureServicesAdder
@@ -12,6 +16,21 @@ namespace Sellify.Infrastructure.ServicesAdder
             });
             services.AddIdentityCore<ApplicationUser>()
                 .AddEntityFrameworkStores<SellifyMicrosoftSqlContext>();
+
+            services.AddAuthentication(o =>
+            {
+                o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o =>
+            {
+                o.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:SecretKey"])),
+                };
+            });
             return services;
         }
     }
