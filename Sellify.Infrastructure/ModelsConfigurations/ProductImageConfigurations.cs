b0ff0c0749ce98ab26cfbd4796d6d30e6ee9b791
Mohamed Ashraf <maelsayed4dv@gmail.com>
2026-03-11 01:40:59 -0700

    added product images configurations - (added) configurations

---
+++ Sellify.Infrastructure/ModelsConfigurations/ProductImageConfigurations.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Sellify.Infrastructure.ModelsConfigurations
+{
+    public class ProductImageConfigurations : IEntityTypeConfiguration<ProductImage>
+    {
+        public void Configure(EntityTypeBuilder<ProductImage> builder)
+        {
+            builder.HasKey(pi => pi.Id);
+            builder.HasIndex(pi => pi.ProductId);
+
+            builder.Property(pi => pi.Url).IsRequired();
+            builder.Property(pi => pi.Description).IsRequired();
+
+        }
+    }
+}
