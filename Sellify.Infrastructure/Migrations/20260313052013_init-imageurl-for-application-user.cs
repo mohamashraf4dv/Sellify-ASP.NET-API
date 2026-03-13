eae4e0493eaa45e9de79c2c169ddd9122cf2331c
Mohamed Ashraf <maelsayed4dv@gmail.com>
2026-03-12 22:21:39 -0700

    added imageurl property to applicationUser - (added) image prop

---
+++ Sellify.Infrastructure/Migrations/20260313052013_init-imageurl-for-application-user.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sellify.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class initimageurlforapplicationuser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageURL",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageURL",
+                table: "AspNetUsers");
+        }
+    }
+}
