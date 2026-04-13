2a0b75488be7686ef9d4215b3014ccf07dbc9c3a
Mohamed Ashraf <maelsayed4dv@gmail.com>
2026-04-12 21:03:36 -0700

    added non generic version of result

--- Sellify.Application/Global/ResultBuilder.cs
+++ Sellify.Application/Global/ResultBuilder.cs
@@ -1,5 +1,6 @@
 ﻿namespace Sellify.Application.Global
 {
+    #region Generic Version
     public sealed class Result<T>
     {
         private T? resultValue = default;
@@ -7,35 +8,35 @@
         private Dictionary<string, HashSet<string>>? resultErrorsKeyValues = null;
         private Dictionary<string, string>? resultErrorsKeyValue = null;
         private Result() { }
-        public sealed class ResultBuilder<T> 
+        public sealed class ResultBuilder<T>
         {
-        private T? value = default ;
-        private int statusCode;
-        private Dictionary<string, HashSet<string>>? errorsKeyValues = null;
-        private Dictionary<string, string>? errorsKeyValue = null;
-        private ResultBuilder() { }
+            private T? value = default;
+            private int statusCode;
+            private Dictionary<string, HashSet<string>>? errorsKeyValues = null;
+            private Dictionary<string, string>? errorsKeyValue = null;
+            private ResultBuilder() { }
 
-        public ResultBuilder<T> SetValue(T value) 
-        {
-            this.value = value;
-            return this;
-        }
-        public ResultBuilder<T> SetStatusCode(int statusCode)
-        {
-            this.statusCode = statusCode;
-            return this;
-        }
-        public ResultBuilder<T> HasManyErrorsPerKey()
-        {
-            errorsKeyValues = new Dictionary<string, HashSet<string>>();
-            return this;
-        }
-        public ResultBuilder<T> HasOneErrorPerKey()
-        {
-            errorsKeyValue = new Dictionary<string, string>();
-            return this;
-        }
-        public ResultBuilder<T> SetOneErrorPerKeyValue(string errorKey, string errorValue)
+            public ResultBuilder<T> SetValue(T value)
+            {
+                this.value = value;
+                return this;
+            }
+            public ResultBuilder<T> SetStatusCode(int statusCode)
+            {
+                this.statusCode = statusCode;
+                return this;
+            }
+            public ResultBuilder<T> HasManyErrorsPerKey()
+            {
+                errorsKeyValues = new Dictionary<string, HashSet<string>>();
+                return this;
+            }
+            public ResultBuilder<T> HasOneErrorPerKey()
+            {
+                errorsKeyValue = new Dictionary<string, string>();
+                return this;
+            }
+            public ResultBuilder<T> SetOneErrorPerKeyValue(string errorKey, string errorValue)
             {
                 if (errorsKeyValue is null)
                     throw new InvalidOperationException();
@@ -44,7 +45,7 @@
 
                 return this;
             }
-        public ResultBuilder<T> SetErrorsPerKeyValue(string errorKey, HashSet<string> errorValue)
+            public ResultBuilder<T> SetErrorsPerKeyValue(string errorKey, HashSet<string> errorValue)
             {
                 if (errorsKeyValues is null)
                     throw new InvalidOperationException();
@@ -54,15 +55,75 @@
                 return this;
             }
 
-        public Result<T> Build()
+            public Result<T> Build()
             {
-                if(errorsKeyValue is not null)
-                    return new Result<T>() { resultValue=value,resultErrorsKeyValue=errorsKeyValue,resultStatusCode=statusCode};
+                if (errorsKeyValue is not null)
+                    return new Result<T>() { resultValue = value, resultErrorsKeyValue = errorsKeyValue, resultStatusCode = statusCode };
 
                 return new Result<T>() { resultValue = value, resultErrorsKeyValues = errorsKeyValues, resultStatusCode = statusCode };
             }
 
         }
     }
+    #endregion
 
+    #region Non-Generic Version (No Value Here)
+    public sealed class Result
+    {
+        private int resultStatusCode;
+        private Dictionary<string, HashSet<string>>? resultErrorsKeyValues = null;
+        private Dictionary<string, string>? resultErrorsKeyValue = null;
+        private Result() { }
+        public sealed class ResultBuilder
+        {
+            private int statusCode;
+            private Dictionary<string, HashSet<string>>? errorsKeyValues = null;
+            private Dictionary<string, string>? errorsKeyValue = null;
+            private ResultBuilder() { }
+
+            public ResultBuilder SetStatusCode(int statusCode)
+            {
+                this.statusCode = statusCode;
+                return this;
+            }
+            public ResultBuilder HasManyErrorsPerKey()
+            {
+                errorsKeyValues = new Dictionary<string, HashSet<string>>();
+                return this;
+            }
+            public ResultBuilder HasOneErrorPerKey()
+            {
+                errorsKeyValue = new Dictionary<string, string>();
+                return this;
+            }
+            public ResultBuilder SetOneErrorPerKeyValue(string errorKey, string errorValue)
+            {
+                if (errorsKeyValue is null)
+                    throw new InvalidOperationException();
+
+                errorsKeyValue.Add(errorKey, errorValue);
+
+                return this;
+            }
+            public ResultBuilder SetErrorsPerKeyValue(string errorKey, HashSet<string> errorValue)
+            {
+                if (errorsKeyValues is null)
+                    throw new InvalidOperationException();
+
+                errorsKeyValues.Add(errorKey, errorValue);
+
+                return this;
+            }
+
+            public Result Build()
+            {
+                if (errorsKeyValue is not null)
+                    return new Result() { resultErrorsKeyValue = errorsKeyValue, resultStatusCode = statusCode };
+
+                return new Result() { resultErrorsKeyValues = errorsKeyValues, resultStatusCode = statusCode };
+            }
+
+        }
+    } 
+    #endregion
 }
